24e230c93f845a717758697d8f4760e854266b91
khodorPavel <47274659+Khodor1997@users.noreply.github.com>
2019-10-08 22:30:24 +0300

    Update guessibg-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.$min = min;
+        this.$max = max;
     }
 
     guess() {
-
+        this.binary = Math.ceil((this.$min + this.$max)/2)
+        return this.binary
     }
 
     lower() {
-
+        this.$max = this.binary
     }
 
     greater() {
-
+        this.$min = this.binary
     }
 }
 
